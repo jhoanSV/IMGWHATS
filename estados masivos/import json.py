91e4ef5ee0828ac244fe1ab0f3aa90c024f67b90
jhoanSV <jhoanjssv97@outlook.com>
2023-07-28 12:56:04 -0500

    Posicionar centro imagen
    Para el procesamiento de imágenes fijas hacemos mejoramos el que pueda posicionar la imagen en el centro de forma automática si así se desea

--- estados masivos/import json.py
+++ estados masivos/import json.py
@@ -2,11 +2,11 @@ import json
 from PIL import Image
 import os 
 
-# JSON image properties
-image_properties_json = '[{"name": "caminar.jpg", "width": 300, "high": 300, "rotate": 90, "x_position": 0, "y_position":0}, {"name": "LOGOS FERRETERIAS", "width": 300, "high": 300, "rotate": 60, "x_position": 0, "y_position":0}]'
-# List of Clients
+# *JSON image properties
+image_properties_json = '[{"name": "caminar.jpg", "width": 300, "high": 300, "rotate": 90, "x_position": 0, "y_position":0, "xCenter": true, "yCenter": true}, {"name": "LOGOS FERRETERIAS", "width": 300, "high": 300, "rotate": 60, "x_position": 0, "y_position":0, "xCenter" : false, "yCenter": false}]'
+# *List of Clients
 CLient = ['17','27','28','46','140','433']
-#Create a background image with the required size 
+# *Create a background image with the required size 
 size = width, height = 3000, 3000
 
 def search_file(folder, filename):
@@ -33,19 +33,22 @@ def properties(image,property):
     new_image.paste(png_image, (0, 0), mask=png_image.split()[3])
     return new_image
 
-background_image = Image.new('RGB',size, 'white')
+# !background_image = Image.new('RGB',size, 'white')
 I_properties = json.loads(image_properties_json)
 
 #For each Client crete a new image
 for cli in CLient:
     #apliying the properties to each image on the list of properties
-    for bla in I_properties:
-        #print(bla['name'])
-        if bla['name'].endswith('jpg') or bla['name'].endswith('png'):
-           image1 = properties(bla['name'],bla)
+    background_image = Image.new('RGB',size, 'white')
+    for ImageProperty in I_properties:
+        # *Variables for each image
+        x = ImageProperty['x_position']
+        y = ImageProperty['y_position']
+        if ImageProperty['name'].endswith('jpg') or ImageProperty['name'].endswith('png'):
+           image1 = properties(ImageProperty['name'],ImageProperty)
         else:
-            fil = search_file(bla['name'],cli)
-            image1 = properties(fil,bla)
+            fil = search_file(ImageProperty['name'],cli)
+            image1 = properties(fil,ImageProperty)
         # Load the PNG image and convert it to RGBA
         #png_image = image1.convert('RGBA')
 
@@ -55,7 +58,13 @@ for cli in CLient:
         # Paste the PNG image on the new RGBA image with the alpha channel as the mask
         #new_image.paste(png_image, (0, 0), mask=png_image.split()[3])
 
-        # Paste the new RGBA image with the PNG image on top of the background image
-        background_image.paste(image1, (bla['x_position'], bla['y_position']), image1)
+        # *Check if the RGBA image have to be at the center in the x position or in the y position
+        if ImageProperty['xCenter'] == True:
+            x = int(round((size[0] - ImageProperty['width'])/2))
+        if ImageProperty['yCenter'] == True:
+            y = int(round((size[1] - ImageProperty['high'])/2))
+
+        # * Paste the new RGBA image with the PNG image on top of the background image
+        background_image.paste(image1, (x, y), image1)
         #print('aqui si entro')
     background_image.save('pruebas/{}.jpg'.format(cli))
